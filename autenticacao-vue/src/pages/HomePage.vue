b306ffc12c92fbf15ce3ce00f0cc4f887f660ecf
belleeferreira <belleesantospb@gmail.com>
2023-07-14 10:20:29 -0300

    consumo de api feita na pagina home.

--- autenticacao-vue/src/pages/HomePage.vue
+++ autenticacao-vue/src/pages/HomePage.vue
@@ -1,13 +1,28 @@
 <template>
     <q-page>
         <p>login sucesso {{ userName }} </p>
+        <div v-for="(pokemon, i) in pokemons" :key="i">
+          {{ pokemons }}
+        </div>
     </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import { api } from '../boot/axios'
 export default defineComponent({
   name: 'HomePage',
+  setup () {
+    const pokemons = ref([])
+    const fetchPokemons = () =>
+      api
+        .get('/pokemon')
+        .then((res) => (pokemons.value = res.data.results))
+        .catch(error => console.log(error))
+    onMounted(fetchPokemons)
+
+    return { pokemons }
+  },
   computed: {
     userName () {
       return localStorage.getItem('username')
